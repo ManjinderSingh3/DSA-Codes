60439bb0c4ee9e63ebdbb1a44ffe9b425b06c2ac
Manjinder <msinghw3@gmail.com>
2025-06-24 16:31:35 -0500

    Maximum depth of Binary Tree

---
+++ a_Trees/src/main/java/DFS/c_MaximumDepthOfBinaryTree.java
@@ -0,0 +1,41 @@
+package main.java.DFS;
+
+public class c_MaximumDepthOfBinaryTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return height(root);
+    }
+
+    int height(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int left = height(node.left);
+        int right = height(node.right);
+
+        return Math.max(left, right) + 1;
+
+    }
+}
