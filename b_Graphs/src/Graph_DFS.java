9193ed177cfb2730264f79d8d46f9a593b8ea0f0
Manjinder <msinghw3@gmail.com>
2025-07-04 18:33:29 -0500

    DFS traversal implementation of undirected graph

--- b_Graphs/src/Graph_DFS.java
+++ b_Graphs/src/Graph_DFS.java
@@ -1,4 +1,72 @@
 import java.util.*;
 public class Graph_DFS {
+    
+    static class Edge {
+        int source;
+        int destination;
+        
+        public Edge(int s, int d){
+            this.source = s;
+            this.destination = d;
+        }
+    }
+    
+    public static void createGraph(ArrayList<Edge> graph[]){
+        for(int i=0;i<graph.length;i++){
+            graph[i] = new ArrayList<Edge>(); 
+        }
+
+        graph[0].add(new Edge(0,1));
+        graph[0].add(new Edge(0,2));
+
+        graph[1].add(new Edge(1,0));
+        graph[1].add(new Edge(1,3));
+
+        graph[2].add(new Edge(2,0));
+        graph[2].add(new Edge(2,4));
+
+
+        graph[3].add(new Edge(3,1));
+        graph[3].add(new Edge(3,4));
+        graph[3].add(new Edge(3,5));
+
+        graph[4].add(new Edge(4,2));
+        graph[4].add(new Edge(4,3));
+        graph[4].add(new Edge(4,5));
+
+        graph[5].add(new Edge(5,3));
+        graph[5].add(new Edge(5,4));
+        graph[5].add(new Edge(5,6));
+
+        graph[6].add(new Edge(6,5));
+    }
+    
+    public static void graphDFS(ArrayList<Edge> graph[], int current, boolean[] visitedArray){
+        if(!visitedArray[current]){
+            System.out.print(current + " ");
+            visitedArray[current] = true;
+            for(int i=0;i<graph[current].size();i++){
+                Edge e = graph[current].get(i);
+                graphDFS(graph, e.destination, visitedArray);
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int V =7;
+        /*
+                1 -------- 3
+               /           |  \
+              0            |   5 ---- 6
+               \           |  /
+                2 -------- 4
+         */
+        ArrayList<Edge> graphArray[] = new ArrayList[V];
+        boolean visitedArray[] = new boolean[V];
+        createGraph(graphArray);
+        graphDFS(graphArray, 0, visitedArray);
+        
+    }
 
 }
