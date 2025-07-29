b7b9e29c200cb6a9a8d3abcf9cd6e73c8c606464
Manjinder <msinghw3@gmail.com>
2025-07-29 18:38:32 -0500

    Min Stack Leetcode problem using two stacks

--- g_Stack/src/b_MinStack.java
+++ g_Stack/src/b_MinStack.java
@@ -1,2 +1,44 @@
+import java.util.Stack;
 public class b_MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+
+    public b_MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        if(stack.isEmpty()){
+            stack.push(val);
+            minStack.push(val);
+            return;
+        }
+        stack.push(val);
+        if( val <= minStack.peek()){
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        if(stack.isEmpty()){
+            return;
+        }
+        int poppedValue = stack.pop();
+        if(poppedValue == minStack.peek()){
+            minStack.pop();
+        }
+
+    }
+    public int top() {
+        return stack.peek();
+    }
+    public int getMin() {
+        return minStack.peek();
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
+
