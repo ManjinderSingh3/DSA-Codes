a063274efe0ae42759d79378c84283f5abde7e7e
Manjinder <msinghw3@gmail.com>
2025-09-06 14:48:35 -0500

    Started recursion playlist and practiced subset questions of String

---
+++ h_Recursion/src/Practice/d_SubsetSubsequenceStringQuestions/a_SkipACharacter.java
@@ -0,0 +1,33 @@
+package Practice.d_SubsetSubsequenceStringQuestions;
+
+public class a_SkipACharacter {
+    // Approach-1 (Without using extra variable to store answer)
+    static String skipACharacter(String str){
+        if(str.isEmpty()){
+            return str;
+        }
+        char ch = str.charAt(0);
+        if(ch=='a'){
+            return skipACharacter(str.substring(1));
+        }else{
+            return ch + skipACharacter(str.substring(1));
+        }
+    }
+
+    static String skipACharacter2(String processedStr, String unprocessedStr){
+        if(unprocessedStr.isEmpty()){
+            return processedStr;
+        }
+        char ch = unprocessedStr.charAt(0);
+        if(ch=='a'){
+            return skipACharacter2(processedStr,unprocessedStr.substring(1));
+        }else{
+            return skipACharacter2(processedStr+ch,unprocessedStr.substring(1));
+        }
+    }
+
+    public static void main(String[] args) {
+        //System.out.println(skipACharacter("abccdah"));
+        System.out.println(skipACharacter2("","abccdah"));
+    }
+}
