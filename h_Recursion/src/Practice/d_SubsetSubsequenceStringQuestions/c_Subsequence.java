593af434f28407884bdbd73e6024f450920470f4
Manjinder <msinghw3@gmail.com>
2025-09-09 16:42:07 -0500

    Subsequence question with ArrayList as return type - Recursive solution

--- h_Recursion/src/Practice/d_SubsetSubsequenceStringQuestions/c_Subsequence.java
+++ h_Recursion/src/Practice/d_SubsetSubsequenceStringQuestions/c_Subsequence.java
@@ -1,5 +1,7 @@
 package Practice.d_SubsetSubsequenceStringQuestions;
 
+import java.util.ArrayList;
+
 public class c_Subsequence {
     public static void subSequence(String processedString, String unprocessedString){
         if(unprocessedString.isEmpty()){
@@ -12,7 +14,37 @@ public class c_Subsequence {
 
     }
 
+    // Return ArrayList by creating a new list inside the function body
+    public static ArrayList<String> subSequenceInList(String processed, String unprocessed){
+        if(unprocessed.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(processed);
+            return list;
+        }
+        char ch = unprocessed.charAt(0);
+        ArrayList<String> left = subSequenceInList(processed+ch, unprocessed.substring(1));
+        ArrayList<String> right = subSequenceInList(processed, unprocessed.substring(1));
+        left.addAll(right);
+        return left;
+    }
+
+
+    // Returning Arraylist by passing in each function call. (One object and multiple reference variables)
+    static ArrayList<String> alist = new ArrayList<String>();
+    public static ArrayList<String> subSequenceInListII(String processed, String unprocessed, ArrayList<String> list){
+        if(unprocessed.isEmpty()){
+            list.add(processed);
+            return list;
+        }
+
+        subSequenceInListII(processed+unprocessed.charAt(0), unprocessed.substring(1), alist);
+        subSequenceInListII(processed,unprocessed.substring(1), alist);
+        return alist;
+    }
+
     public static void main(String[] args) {
         subSequence("", "abc");
+        System.out.println(subSequenceInList("","abc"));
+        System.out.println(subSequenceInListII("","abc", alist));
     }
 }
